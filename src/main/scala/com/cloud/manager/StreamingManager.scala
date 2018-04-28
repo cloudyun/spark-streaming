67b12652fcaa09619cff4ec6e154465ece25f335
高云 <gaoyun@netposa.com>
2018-04-28 17:38:20 +0800

    modify

--- src/main/scala/com/cloud/manager/StreamingManager.scala
+++ src/main/scala/com/cloud/manager/StreamingManager.scala
@@ -5,50 +5,41 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.Durations
 import org.apache.spark.streaming.StreamingContext
 
-class StreamingManager  private(taskName:String){
-    //获取sparkstreaming
-    val sparkConf = new SparkConf().setAppName("vidServer"+taskName)
-    
-    sparkConf.set("spark.streaming.unpersist", "true")
-    
-    //设置一个批次从kafka拉取的数据
-    sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "500")
-    
-    sparkConf.setMaster("local[3]")
-    
-    sparkConf.set("spark.default.parallelism", "150")
-    
-    val ssc = new StreamingContext(sparkConf, Durations.seconds(10000))
-    
-//    val brokers=LocalSystemConfig.getInstance().getKafkaConfig.getKfkBrokerList
-    
-    val brokers="yh-ambari01.lingda.com:6667,yh-ambari02.lingda.com:6667,yh-ambari03.lingda.com:6667"
-      
-//    val zkHost=LocalSystemConfig.getInstance().getKafkaConfig.getZKConnect
-    
-    val zkHost="yh-ambari03.lingda.com:2181,yh-ambari01.lingda.com:2181,yh-ambari02.lingda.com:2181"
-    
-    val zkClient = new ZkClient(zkHost)
-    
-    def sscRun()={
-      //开启
-      ssc.start()
-      //等待
-      ssc.awaitTermination()
-    }
-    
+class StreamingManager private (taskName: String) {
+  //获取sparkstreaming
+  val sparkConf = new SparkConf().setAppName("vidServer" + taskName)
+
+  sparkConf.set("spark.streaming.unpersist", "true")
+
+  //设置一个批次从kafka拉取的数据
+  sparkConf.set("spark.streaming.kafka.maxRatePerPartition", "50")
+
+  sparkConf.setMaster("local[3]")
+
+  sparkConf.set("spark.default.parallelism", "150")
+
+  val ssc = new StreamingContext(sparkConf, Durations.seconds(10000))
+
+  val brokers = "yh-ambari01.lingda.com:6667,yh-ambari02.lingda.com:6667,yh-ambari03.lingda.com:6667"
+
+  val zkHost = "yh-ambari03.lingda.com:2181,yh-ambari01.lingda.com:2181,yh-ambari02.lingda.com:2181"
+
+  val zkClient = new ZkClient(zkHost)
+
+  def sscRun() = {
+    ssc.start()//开启
+    ssc.awaitTermination()//等待
+  }
 }
-object StreamingManager{
-  
-  private var streamingManager:StreamingManager = null
-  
-  def getInstance(taskName:String):StreamingManager={
-    
-    if(streamingManager==null)
-    {
-    	streamingManager = new StreamingManager(taskName);
+
+object StreamingManager {
+
+  private var streamingManager: StreamingManager = null
+
+  def getInstance(taskName: String): StreamingManager = {
+    if (streamingManager == null) {
+      streamingManager = new StreamingManager(taskName);
     }
     streamingManager
   }
-  
 }
